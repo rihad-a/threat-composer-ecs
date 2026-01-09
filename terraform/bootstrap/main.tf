e0d76feb0905e2ef889adfc03bc39b4c018b6d4e
rihad-a <Rihadwork@hotmail.com>
2026-01-09 12:53:14 +0000

    Adding s3 versioning and access block. And removing route53 data block

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -4,28 +4,39 @@ resource "aws_s3_bucket" "s3" {
   bucket = "rihads3"
 }
 
+resource "aws_s3_bucket_versioning" "rihads3" {
+  bucket = aws_s3_bucket.s3.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_s3_bucket_public_access_block" "rihads3" {
+  bucket = aws_s3_bucket.s3.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 # Hosted Zone Creation
 
 resource "aws_route53_zone" "networking" {
   name = "networking.rihad.co.uk"
  }
 
-data "aws_route53_zone" "networking" {
-  name = "networking.rihad.co.uk"
- }
-
 output "ns" {
-  value = "${data.aws_route53_zone.networking.name_servers}"
+  value = "${aws_route53_zone.networking.name_servers}"
  }
 
 ## Linkage Of Route53 Hosted Zone NS' to Cloudflare
 
  resource "cloudflare_dns_record" "route53-ns" {
   name    = "networking"
-  count   = "${length(data.aws_route53_zone.networking.name_servers)}"
+  count   = "${length(aws_route53_zone.networking.name_servers)}"
   ttl     = 300
   type    = "NS"
-  content   = "${element(data.aws_route53_zone.networking.name_servers, count.index)}"
+  content   = "${element(aws_route53_zone.networking.name_servers, count.index)}"
   zone_id = "415c05da9144abf5a32a57c25dfefe06"
 }
 
